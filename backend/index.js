35346f9ebd791ddd8569d099055d9a2fed7a3641
Tushar Singh <104367606+tstar28@users.noreply.github.com>
2023-09-28 19:21:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,9 @@ const cors = require("cors");
 const passport = require("passport");
 const authRoute = require('./routes/auth');
 const app = express();
-
+require('dotenv').config();
 app.use(
-  cookieSession({ name: "session", keys: ["lama"], maxAge: 24 * 60 * 60 * 100 })
+  cookieSession({ name: "session", keys: [process.env.COOKIE_KEY], maxAge: 24 * 60 * 60 * 100 })
 );
 
 app.use(passport.initialize());
